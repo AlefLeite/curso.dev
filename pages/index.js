91e207ffda671a84d4d33fe1525731f8792b09cd
Alef Perez Leite <alefperezleite09@gmail.com>
2025-11-03 19:29:38 -0300

    Mensagem para meu amor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Teste de segundo deploy!</h1>
+    return <h1>Só passando pra te dizer que te amo muuuuuuitão! coloquei uma musica nova na playlist ❤️</h1>
 }
 
 export default Home
