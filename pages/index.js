d3d687c5c69b37aed5f71dc58316d49311a8612b
Alef Perez Leite <alefperezleite09@gmail.com>
2025-11-11 23:14:08 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home (){
-    return <h1>Só passando pra te dizer que te amo muuuuuuitão! coloquei uma musica nova na playlist ❤️</h1>
+function Home() {
+  return (
+    <h1>
+      Só passando pra te dizer que te amo muuuuuuitão! coloquei uma musica nova
+      na playlist ❤️
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
